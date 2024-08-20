e2d12cd332d7d5dff7762e3982ec6f2f1cec2334
CopperyTexas <dengleboff@yandex.ru>
2024-08-20 23:50:38 +0500

    страница настроек

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LayoutComponent } from './common-ui/layout/layout.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { SettingsPageComponent } from './pages/settings/settings.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 export const routes: Routes = [
   {

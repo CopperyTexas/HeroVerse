d4651b78baca36212fc4200ff128c65779982168
Denis Glebov <dengleboff@yandex.ru>
2024-09-05 17:21:53 +0500

    input

---
+++ src/app/common-ui/avatar-circle/avatar-circle.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ProfileService } from '../../data/services/profile.service';
+
+@Component({
+  selector: 'app-avatar-circle',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './avatar-circle.component.html',
+  styleUrl: './avatar-circle.component.scss',
+})
+export class AvatarCircleComponent implements OnInit {
+  me: Profile | null = null;
+  private route = inject(ActivatedRoute);
+
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.me = this.profileService.me;
+  }
+  profile$: Observable<Profile | null> = this.route.paramMap.pipe(
+    switchMap((params) => {
+      const id = params.get('id');
+      return id
+        ? this.profileService.getAccount(id)
+        : this.profileService.getMe();
+    })
+  );
+}
--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Renderer2, inject } from '@angular/core';
+import { AvatarCircleComponent } from '../../../common-ui/avatar-circle/avatar-circle.component';
 
 @Component({
   selector: 'app-post-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AvatarCircleComponent],
   templateUrl: './post-input.component.html',
-  styleUrl: './post-input.component.scss'
+  styleUrl: './post-input.component.scss',
 })
 export class PostInputComponent {
-
+  r2 = inject(Renderer2);
+  onTextAreaInput(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+    this.r2.setStyle(textarea, 'height', 'auto');
+    this.r2.setStyle(textarea, 'height', textarea.scrollHeight + 'px');
+  }
 }
